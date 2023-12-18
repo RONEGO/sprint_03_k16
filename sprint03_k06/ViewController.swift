0b92fbbfb22419ab9cdc8301c76464ec749fadfa
ron3go <ron3go@yandex-team.ru>
2023-12-18 20:00:04 +0300

    Adding new files

---
+++ sprint03_k06/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  sprint03_k06
+//
+//  Created by Egor Geronin on 18.12.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
